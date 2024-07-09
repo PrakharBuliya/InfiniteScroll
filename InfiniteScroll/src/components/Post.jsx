bfac29ea7949a851971618c6dee16a86e0c4bc44
Prakhar Buliya <prakharbuliya02@gmail.com>
2024-07-09 14:11:49 +0530

    Infinite Scroll

---
+++ InfiniteScroll/src/components/Post.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import "../App.css";
+
+const Post = ({ data, setPageNo }) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver((param) => {
+      console.log(param);
+      if (param[0].isIntersecting) {
+        observer.unobserve(lastImage);
+        setPageNo((pageNo) => pageNo + 1);
+      }
+    });
+
+    const lastImage = document.querySelector(".image-post:last-child");
+
+    console.log(lastImage, "Last Image-----");
+
+    if (!lastImage) {
+      return;
+    }
+    observer.observe(lastImage);
+  }, [data]);
+
+  return (
+    <div className="container">
+      {data.map((item, index) => {
+        return (
+          <img
+            className="image-post"
+            key={item.id}
+            src={item.download_url}
+            alt=""
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Post;
